6e73f53f3aea0838a8c16dfbfaa2376fa0d8e8bf
Color0525 <rio.game.kojima@gmail.com>
2021-08-21 06:52:22 +0900

    出た数が赤くなるように。出た数がわかるように

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public int Number { get; set; }
+    public bool Checked { get; set; } = false;
+}
--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class Gamemanager : MonoBehaviour
 {
     [SerializeField] GameObject m_cellPrefab = default;
-    [SerializeField] Transform m_cellPanel = default;
+    [SerializeField] Transform m_cardPanel = default;
+    [SerializeField] Transform m_numberPanel = default;
 
+    List<Cell> m_cardCells = new List<Cell>();
+    List<int> m_allnumbers = new List<int>();
     List<int> m_columnB = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
     List<int> m_columnI = new List<int>() { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
     List<int> m_columnN = new List<int>() { 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
@@ -16,41 +19,63 @@ public class Gamemanager : MonoBehaviour
 
     private void Start()
     {
+        for (int i = 0; i < 75; i++)
+        {
+            m_allnumbers.Add(i + 1);
+        }
+
         for (int i = 0; i < 25; i++)
         {
-            GameObject go = Instantiate(m_cellPrefab, m_cellPanel);
+            GameObject go = Instantiate(m_cellPrefab, m_cardPanel);
+            Cell newCell = go.GetComponent<Cell>();
+            m_cardCells.Add(newCell);
             int num = default;
             if (i < 5)
             {
-                int ran = Random.Range(0, m_columnB.Count);
-                num = m_columnB[ran];
-                m_columnB.RemoveAt(ran);
+                num = GetRandomNamber(m_columnB);
             }
             else if (i < 10)
             {
-                int ran = Random.Range(0, m_columnI.Count);
-                num = m_columnI[ran];
-                m_columnI.RemoveAt(ran);
+                num = GetRandomNamber(m_columnI);
             }
             else if (i < 15)
             {
-                int ran = Random.Range(0, m_columnN.Count);
-                num = m_columnN[ran];
-                m_columnN.RemoveAt(ran);
+                num = GetRandomNamber(m_columnN);
             }
             else if (i < 20)
             {
-                int ran = Random.Range(0, m_columnG.Count);
-                num = m_columnG[ran];
-                m_columnG.RemoveAt(ran);
+                num = GetRandomNamber(m_columnG);
             }
             else if (i < 25)
             {
-                int ran = Random.Range(0, m_columnO.Count);
-                num = m_columnO[ran];
-                m_columnO.RemoveAt(ran);
+                num = GetRandomNamber(m_columnO);
             }
+            newCell.Number = num;
             go.GetComponentInChildren<Text>().text = num.ToString();
         }
+    }
+
+    public void StartRoulette()
+    {
+        GameObject go = Instantiate(m_cellPrefab, m_numberPanel);
+        int num = GetRandomNamber(m_allnumbers);
+        go.GetComponentInChildren<Text>().text = num.ToString();
+        //チェック
+        foreach (var cell in m_cardCells)
+        {
+            if (cell.Number == num)
+            {
+                cell.Checked = true;
+                cell.gameObject.GetComponent<Image>().color = Color.red;
+            }
+        }
+    }
+
+    int GetRandomNamber(List<int> nambers)
+    {
+        int ran = Random.Range(0, nambers.Count);
+        int num = nambers[ran];
+        nambers.RemoveAt(ran);
+        return num;
     }
 }
